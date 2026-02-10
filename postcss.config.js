32a5ca9e645f4737f46bc6de59a0a8e45a11c7cf
manumontaraz <montarazmanu@gmail.com>
2026-02-10 19:06:35 +0100

    added minify to js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-const isProduction = process.env.NODE_ENV === 'production'
-
 module.exports = {
   plugins: [
     require('postcss-nested'),
-    ...(isProduction ? [require('cssnano')({ preset: 'default' })] : [])
+    require('cssnano')
   ]
 }
